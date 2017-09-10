74cf0c1b8103ec69cb01db65d85f6c7b48597a3d
jbayona87 <johnny.bayona.avalos@gmail.com>
2017-09-10 12:35:59 -0500

    commit inicial

---
+++ Ejercicio/DAL/ServicioDeUsuarios.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio.DAL
+{
+    public class ServicioDeUsuarios
+    {
+        private SqlConnection iniciarConexion()
+        {
+            SqlConnection conection = new SqlConnection();
+            conection.ConnectionString = ConfigurationManager.ConnectionStrings["TIENDA"].ConnectionString;
+            return conection;
+        }
+        public void CrearUsuario(NuevoUsuario Usuario)
+        {
+            SqlConnection conection = this.iniciarConexion();
+            SqlCommand comando = new SqlCommand("CREARUSUARIO", conection);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.Add(new SqlParameter("@Name", Usuario.Name));
+            comando.Parameters.Add(new SqlParameter("@Clave", Usuario.Clave));
+            comando.Parameters.Add(new SqlParameter("@Enable", Usuario.Enable));
+            conection.Open();
+            comando.ExecuteNonQuery();
+            conection.Close();
+        }
+        public List<NuevoUsuario> ListarUsuario()
+        {
+            SqlConnection conection = this.iniciarConexion();
+            SqlCommand comando = new SqlCommand("LISTARUSUARIO", conection);
+            comando.CommandType = CommandType.StoredProcedure;
+            conection.Open();
+            SqlDataReader lectorDeDatos = comando.ExecuteReader();
+            List<NuevoUsuario> usuarios = new List<NuevoUsuario>();
+            while (lectorDeDatos.Read())
+            {
+                NuevoUsuario usuario = new NuevoUsuario();
+                usuario.Id = (int)lectorDeDatos.GetInt32(0);
+                usuario.Name = lectorDeDatos.GetString(1);
+                usuario.Clave = lectorDeDatos.GetString(2);
+                usuario.Enable = (Boolean)lectorDeDatos.GetBoolean(3);
+                usuarios.Add(usuario);
+            }
+            conection.Close();
+            return usuarios;
+        }
+        public void EliminarUsuario(int Id)
+        {
+            SqlConnection conection = this.iniciarConexion();
+            SqlCommand comando = new SqlCommand("ELIMINAR_USUARIO", conection);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.Add(new SqlParameter("@Id", Id));
+            
+            conection.Open();
+            comando.ExecuteNonQuery();
+            conection.Close();
+        }
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Ejercicio.DAL;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioDeUsuarios servicioUsuario = new ServicioDeUsuarios();
+            NuevoUsuario nuevoUsuario = new NuevoUsuario();
+            nuevoUsuario.Name = "VICTOR";
+            nuevoUsuario.Clave = "12345";
+            nuevoUsuario.Enable = true;
+            servicioUsuario.CrearUsuario(nuevoUsuario);
+
+          
+            servicioUsuario.EliminarUsuario(2);
+
+            servicioUsuario.ListarUsuario().ForEach(usuario => Console.WriteLine(usuario.Name));
+            Console.ReadKey();
+        }
+    }
+}
